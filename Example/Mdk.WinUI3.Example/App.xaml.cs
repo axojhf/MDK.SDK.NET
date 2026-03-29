a37618c2d06138aad5ba4670fed0a1969a541016
Axojhf <20625305+axojhf@users.noreply.github.com>
2026-03-30 00:41:06 +0800

    fix: use file-scoped namespaces

--- Example/Mdk.WinUI3.Example/App.xaml.cs
+++ Example/Mdk.WinUI3.Example/App.xaml.cs
@@ -5,41 +5,40 @@ using Microsoft.UI.Xaml;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace Mdk.WinUI3.Example
+namespace Mdk.WinUI3.Example;
+
+/// <summary>
+/// Provides application-specific behavior to supplement the default Application class.
+/// </summary>
+public partial class App : Application
 {
     /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
+    /// Initializes the singleton application object.  This is the first line of authored code
+    /// executed, and as such is the logical equivalent of main() or WinMain().
     /// </summary>
-    public partial class App : Application
+    public App()
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
-        public App()
+        this.InitializeComponent();
+        this.UnhandledException += (sender, e) =>
         {
-            this.InitializeComponent();
-            this.UnhandledException += (sender, e) =>
-            {
-                var logPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "Mdk.WinUI3.Example.Crash.log");
-                using var writer = new StreamWriter(logPath, true);
-                writer.WriteLine($"[{DateTime.Now}]");
-                writer.WriteLine($"Message: {e.Exception.Message}");
-                writer.WriteLine($"StackTrace: ");
-                writer.WriteLine(e.Exception.StackTrace);
-            };
-        }
-
-        /// <summary>
-        /// Invoked when the application is launched.
-        /// </summary>
-        /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
-        {
-            m_window = new MainWindow();
-            m_window.Activate();
-        }
-
-        private Window? m_window;
+            var logPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "Mdk.WinUI3.Example.Crash.log");
+            using var writer = new StreamWriter(logPath, true);
+            writer.WriteLine($"[{DateTime.Now}]");
+            writer.WriteLine($"Message: {e.Exception.Message}");
+            writer.WriteLine($"StackTrace: ");
+            writer.WriteLine(e.Exception.StackTrace);
+        };
     }
+
+    /// <summary>
+    /// Invoked when the application is launched.
+    /// </summary>
+    /// <param name="args">Details about the launch request and process.</param>
+    protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+    {
+        m_window = new MainWindow();
+        m_window.Activate();
+    }
+
+    private Window? m_window;
 }
--- Example/Mdk.WinUI3.Example/MainWindow.xaml.cs
+++ Example/Mdk.WinUI3.Example/MainWindow.xaml.cs
@@ -7,56 +7,55 @@ using WinRT;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace Mdk.WinUI3.Example
+namespace Mdk.WinUI3.Example;
+
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
+    private readonly MDKPlayer _player = new();
+    private readonly D3D11RenderAPI _ra = new();
+    public MainWindow()
     {
-        private readonly MDKPlayer _player = new();
-        private readonly D3D11RenderAPI _ra = new();
-        public MainWindow()
-        {
-            this.InitializeComponent();
-            _player.SetDecoders(MediaType.Video, ["MFT:d3d=11", "hap", "D3D11", "DXVA", "FFmpeg"]);
-            _player.OnMediaStatus((old, @new) => old == @new || @new != MediaStatus.Loaded || true);
-            var vid = ((IWinRTObject)swapChainPanel).NativeObject;
-            _player.SetRenderAPI(_ra, vid.ThisPtr);
-            _player.UpdateNativeSurface(vid.ThisPtr);
-            _player.Set(ColorSpace.ColorSpaceUnknown);
-        }
+        this.InitializeComponent();
+        _player.SetDecoders(MediaType.Video, ["MFT:d3d=11", "hap", "D3D11", "DXVA", "FFmpeg"]);
+        _player.OnMediaStatus((old, @new) => old == @new || @new != MediaStatus.Loaded || true);
+        var vid = ((IWinRTObject)swapChainPanel).NativeObject;
+        _player.SetRenderAPI(_ra, vid.ThisPtr);
+        _player.UpdateNativeSurface(vid.ThisPtr);
+        _player.Set(ColorSpace.ColorSpaceUnknown);
+    }
 
-        // Play
-        private void Play(object? sender, RoutedEventArgs e)
-        {
-            var state = _player.State;
-            _player.Set(state == PlaybackState.NotRunning ? State.Playing : State.Stopped);
-        }
+    // Play
+    private void Play(object? sender, RoutedEventArgs e)
+    {
+        var state = _player.State;
+        _player.Set(state == PlaybackState.NotRunning ? State.Playing : State.Stopped);
+    }
 
-        // Select File
-        private async void SelectFileAsync(object? sender, RoutedEventArgs e)
-        {
+    // Select File
+    private async void SelectFileAsync(object? sender, RoutedEventArgs e)
+    {
 
-            var picker = new FileOpenPicker();
-            picker.FileTypeFilter.Add(".mp4");
-            picker.FileTypeFilter.Add(".avi");
-            picker.FileTypeFilter.Add(".mkv");
-            picker.FileTypeFilter.Add(".wmv");
-            picker.FileTypeFilter.Add(".mov");
-            picker.FileTypeFilter.Add(".flv");
-            picker.FileTypeFilter.Add(".webm");
-            picker.FileTypeFilter.Add(".mkv");
-            picker.FileTypeFilter.Add(".mp3");
-            picker.FileTypeFilter.Add(".m4a");
-            picker.FileTypeFilter.Add(".flac");
-            picker.FileTypeFilter.Add(".wav");
-            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
-            WinRT.Interop.InitializeWithWindow.Initialize(picker, hwnd);
-            var file = await picker.PickSingleFileAsync();
-            if (file == null) return;
-            _player.SetMedia(file.Path);
-            FilePath.Text = file.Path;
-        }
+        var picker = new FileOpenPicker();
+        picker.FileTypeFilter.Add(".mp4");
+        picker.FileTypeFilter.Add(".avi");
+        picker.FileTypeFilter.Add(".mkv");
+        picker.FileTypeFilter.Add(".wmv");
+        picker.FileTypeFilter.Add(".mov");
+        picker.FileTypeFilter.Add(".flv");
+        picker.FileTypeFilter.Add(".webm");
+        picker.FileTypeFilter.Add(".mkv");
+        picker.FileTypeFilter.Add(".mp3");
+        picker.FileTypeFilter.Add(".m4a");
+        picker.FileTypeFilter.Add(".flac");
+        picker.FileTypeFilter.Add(".wav");
+        var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+        WinRT.Interop.InitializeWithWindow.Initialize(picker, hwnd);
+        var file = await picker.PickSingleFileAsync();
+        if (file == null) return;
+        _player.SetMedia(file.Path);
+        FilePath.Text = file.Path;
     }
 }
