0edaa10cf2934aaefb04d2fa37e3c9482f021234
Axojhf <20625305+axojhf@users.noreply.github.com>
2026-07-11 21:47:24 +0800

    update: update mdk to 0.37.0. update deps

--- src/Gen/global.cs
+++ src/Gen/global.cs
@@ -242,13 +242,13 @@ namespace MDK.SDK.NET.Gen
         [NativeTypeName("#define MDK_MAJOR 0")]
         internal const int MDK_MAJOR = 0;
 
-        [NativeTypeName("#define MDK_MINOR 36")]
-        internal const int MDK_MINOR = 36;
+        [NativeTypeName("#define MDK_MINOR 37")]
+        internal const int MDK_MINOR = 37;
 
         [NativeTypeName("#define MDK_MICRO 0")]
         internal const int MDK_MICRO = 0;
 
         [NativeTypeName("#define MDK_VERSION MDK_VERSION_INT(MDK_MAJOR, MDK_MINOR, MDK_MICRO)")]
-        internal const int MDK_VERSION = (((0 & 0xff) << 16) | ((36 & 0xff) << 8) | (0 & 0xff));
+        internal const int MDK_VERSION = (((0 & 0xff) << 16) | ((37 & 0xff) << 8) | (0 & 0xff));
     }
 }
